06f15bd017cd3b54f81eb40b1a19d2f4f4a392fb
hibby50 <hibby50@gmail.com>
2020-06-14 13:24:37 +0000

    Add more verbose output and removal of duplicates in playlists

--- simple-playlist-maker.py
+++ simple-playlist-maker.py
@@ -18,20 +18,16 @@ ytmusic = YTMusic('headers_auth.json')
 playlist_files = subprocess.check_output('find %s -name "*m3u*"' % config['playlists']['local'], shell=True)
 playlist_files = playlist_files.decode("utf-8").splitlines()
 
-remote_songs = ytmusic.get_library_upload_songs(99999)
+remote_songs = ytmusic.get_library_upload_songs(300)
 remote_playlists = ytmusic.get_library_playlists()
 print(len(remote_songs))
 playlist_content = {}
 
 for playlist in playlist_files:
     f = open(playlist, "r")
-#    playlist_content[playlist.strip(".m3u")] = f.readlines()
     for remote_playlist in remote_playlists:
         if remote_playlist["title"] in playlist:
             playlist_content[remote_playlist["playlistId"]] = f.readlines()
-
-#for music_tag in music_tags:
-#    print ( music_tag.genre)
 
 for playlist in playlist_content:
     upload = []
@@ -39,12 +35,17 @@ for playlist in playlist_content:
     pl_trackid = []
     for track in pl_tracks:
         pl_trackid.append(track["videoId"])
+    duplicates = [song for song in pl_tracks if pl_trackid.count(song["videoId"]) > 1]
     for song in remote_songs:
         #print(str(song["title"]) + str(playlist_content[playlist]))
         if song["artist"] and song["album"] and song["title"]:
             upload += [song["videoId"] for pl_entry in playlist_content[playlist] if song["title"] in pl_entry \
                 and song["artist"] in pl_entry and song["album"] in pl_entry and \
                 song["videoId"] not in pl_trackid]
+    print("removing %s duplicates from %s" % (len(duplicates), playlist))
+    if len(duplicates) > 0:
+       print(ytmusic.remove_playlist_items(playlist, duplicates))
+    print("adding %s songs to %s" % (len(upload), playlist))
     print(ytmusic.add_playlist_items(playlist, upload))
 #        if song["title"] in playlist_content[playlist]:
 #            print(song["title"] + playlist_content[playlist])
